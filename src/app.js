4e5a91919f780dadecf481d2f4f28d5d6cc62cf5
yeganehradfar <yeganehrdr@gmail.com>
2022-05-25 17:03:15 +0430

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -48,7 +48,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
   }
   
-  let apiKey = "250e54ac74f3957cce92369e1c9b5b96";
-  let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  function search(city) {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+  }
+  
+  search("New York");
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
